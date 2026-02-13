29fb7263e998c8ec4167c585f5634d28ca701f59
CelesteMyst <persomrandom7@gmail.com>
2026-02-12 21:01:30 -0800

    moved movement to one joystick, increased robot speed, and "fixed" robot slowly going left/right when moving forward

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,7 +53,7 @@ public final class Constants {
 
     // This value is multiplied by the joystick value when rotating the robot to
     // help avoid turning too fast and beign difficult to control
-    public static final double DRIVE_SCALING = .7;
+    public static final double DRIVE_SCALING = .9;
     public static final double ROTATION_SCALING = .8;
   }
 }
